f262d2198b7880ffde3ecae788cabe22b06709c1
Sirjay009 Obutte <obuttechima@rocketmail.com>
2024-08-10 23:43:04 +0000

    Edit choice-container, write script.js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,21 @@
-//Get images from folder and set them in an array
-imageArray = [
-    "AlissonBecker.jpg",
-    "AndréOnana.jpg",
-    "AntoineGriezmann.jpg",
-    "AntonioRüdiger.jpg",
-    "BernardoSilva.png",
-    "BukayoSaka.jpg",
-    "CristianoRonaldo.jpg",
-    "DeclanRice.jpg",
-    "ErlingHaaland.jpg",
-    "FlorianWirtz.jpg",
-    "HarryKane.jpg",
-    "JamalMusiala.jpg",
-    "JudeBellingham.jpg",
-    "KevinDeBruyne.jpg",
-    "KylianMbappe.jpg",
-    "LamineYamal.jpg",
-    "LionelMessi.jpg",
-    "LukaModric.jpg",
-    "MartinOdegaard.jpg",
-    "MohamedSalah.jpg",
-    "PhilFoden.jpg",
-    "RobertLewandowski.jpg",
-    "Rodri.jpg",
-    "SadioMané.jpg",
-    "SonHeung-min.png",
-    "ToniKroos.jpg",
-    "VictorOsimhen.jpg",
-    "VigilvanDijk.jpg",
-    "VincentAboubakar.jpg",
-    "ViniciusJr"
-]
+const imageElement = document.getElementById('soccer-image');
+const options = document.querySelectorAll('.option');
+const scoreElement = document.getElementById('score');
 
-/**
- * Create a random image function
- * Get a random index
- * Select a random image from the random index
- * Display the image
-*/
+let score = 0;
+let currentQuestion;
 
-function getRandomImg() {
-    randomIndex = Math.floor(Math.random() * imageArray.length);
-    selectedImage = imageArray[randomIndex];
-    document.getElementById("image-shower").src = `./images/${selectedImage}`;
-}
+// Array of questions
+const questions = [
+    {
+        image: 'AlissonBecker.jpg',
+        correctAnswer: 'option 2',
+        options: ['Option 1', 'Option 2', 'Option 3']
+    },
+
+    {
+        image: 'AndréOnana.jpg',
+        correctAnswer: 'option 1',
+        options: ['Option 1', 'Option 2', 'Option 3']  
+    }
+];
