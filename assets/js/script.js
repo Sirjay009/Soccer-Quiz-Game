84f35adab2f5a6d3a71dbf67466bb28ed4222e05
Chima Obutte <obuttechima@rocketmail.com>
2024-08-13 04:38:10 +0000

    Edit welcome-page container, declare constants for DOM elements in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,143 @@
 //Define player data array
-const players = [
-    { name: "Alisson Becker", img: "assets/images/AlissonBecker.jpg" },
-    { name: "André Onana", img: "assets/images/AndréOnana.jpg" },
-    { name: "Antoine Griezmann", img: "assets/images/AntoineGriezmann.jpg" },
-    { name: "AntonioRüdiger", img: "assets/images/AntonioRüdiger.jpg" },
-    { name: "Bernardo Silva", img: "assets/images/BernardoSilva.png" },
-    { name: "Bukayo Saka", img: "assets/images/BukayoSaka.jpg" },
-    { name: "Cristiano Ronaldo", img: "assets/images/CristianoRonaldo.jpg" },
-    { name: "Declan Rice", img: "assets/images/DeclanRice.jpg" },
-    { name: "Erling Haaland", img: "assets/images/ErlingHaaland.jpg" },
-    { name: "Florian Wirtz", img: "assets/images/FlorianWirtz.jpg" },
-    { name: "Harry Kane", img: "assets/images/HarryKane.jpg" },
-    { name: "Jamal Musiala", img: "assets/images/JamalMusiala.jpg" },
-    { name: "Jude Bellingham", img: "assets/images/JudeBellingham.jpg" },
-    { name: "Kevin De Bruyne", img: "assets/images/KevinDeBruyne.jpg" },
-    { name: "Kylian Mbappé", img: "assets/images/KylianMbappe.jpg" },
-    { name: "Lamine Yamal", img: "assets/images/LamineYamal.jpg" },
-    { name: "Lionel Messi", img: "assets/images/LionelMessi.jpg" },
-    { name: "Luka Modrić", img: "assets/images/LukaModric.jpg" },
-    { name: "Martin Ødegaard", img: "assets/images/MartinOdegaard.jpg" },
-    { name: "Mohamed Salah", img: "assets/images/MohamedSalah.jpg" },
-    { name: "Phil Foden", img: "assets/images/PhilFoden.jpg" },
-    { name: "Robert Lewandowski", img: "assets/images/RobertLewandowski.jpg" },
-    { name: "Rodri", img: "assets/images/Rodri.jpg" },
-    { name: "Sadio Mané", img: "assets/images/SadioMané.jpg" },
-    { name: "Son Heung-min", img: "assets/images/SonHeung-min.png" },
-    { name: "Toni Kroos", img: "assets/images/ToniKroos.jpg" },
-    { name: "Victor Osimhen", img: "assets/images/VictorOsimhen.jpg" },
-    { name: "Vigil van Dijk", img: "assets/images/VigilvanDijk.jpg" },
-    { name: "Vincent Aboubakar", img: "assets/images/VincentAboubakar.jpg" },
-    { name: "Vinicius Jr", img: "assets/images/ViniciusJr.jpg" },
-  ];
+const players = [{
+    name: "Alisson Becker",
+    img: "assets/images/AlissonBecker.jpg"
+  },
+  {
+    name: "André Onana",
+    img: "assets/images/AndréOnana.jpg"
+  },
+  {
+    name: "Antoine Griezmann",
+    img: "assets/images/AntoineGriezmann.jpg"
+  },
+  {
+    name: "AntonioRüdiger",
+    img: "assets/images/AntonioRüdiger.jpg"
+  },
+  {
+    name: "Bernardo Silva",
+    img: "assets/images/BernardoSilva.png"
+  },
+  {
+    name: "Bukayo Saka",
+    img: "assets/images/BukayoSaka.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    img: "assets/images/CristianoRonaldo.jpg"
+  },
+  {
+    name: "Declan Rice",
+    img: "assets/images/DeclanRice.jpg"
+  },
+  {
+    name: "Erling Haaland",
+    img: "assets/images/ErlingHaaland.jpg"
+  },
+  {
+    name: "Florian Wirtz",
+    img: "assets/images/FlorianWirtz.jpg"
+  },
+  {
+    name: "Harry Kane",
+    img: "assets/images/HarryKane.jpg"
+  },
+  {
+    name: "Jamal Musiala",
+    img: "assets/images/JamalMusiala.jpg"
+  },
+  {
+    name: "Jude Bellingham",
+    img: "assets/images/JudeBellingham.jpg"
+  },
+  {
+    name: "Kevin De Bruyne",
+    img: "assets/images/KevinDeBruyne.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    img: "assets/images/KylianMbappe.jpg"
+  },
+  {
+    name: "Lamine Yamal",
+    img: "assets/images/LamineYamal.jpg"
+  },
+  {
+    name: "Lionel Messi",
+    img: "assets/images/LionelMessi.jpg"
+  },
+  {
+    name: "Luka Modrić",
+    img: "assets/images/LukaModric.jpg"
+  },
+  {
+    name: "Martin Ødegaard",
+    img: "assets/images/MartinOdegaard.jpg"
+  },
+  {
+    name: "Mohamed Salah",
+    img: "assets/images/MohamedSalah.jpg"
+  },
+  {
+    name: "Phil Foden",
+    img: "assets/images/PhilFoden.jpg"
+  },
+  {
+    name: "Robert Lewandowski",
+    img: "assets/images/RobertLewandowski.jpg"
+  },
+  {
+    name: "Rodri",
+    img: "assets/images/Rodri.jpg"
+  },
+  {
+    name: "Sadio Mané",
+    img: "assets/images/SadioMané.jpg"
+  },
+  {
+    name: "Son Heung-min",
+    img: "assets/images/SonHeung-min.png"
+  },
+  {
+    name: "Toni Kroos",
+    img: "assets/images/ToniKroos.jpg"
+  },
+  {
+    name: "Victor Osimhen",
+    img: "assets/images/VictorOsimhen.jpg"
+  },
+  {
+    name: "Vigil van Dijk",
+    img: "assets/images/VigilvanDijk.jpg"
+  },
+  {
+    name: "Vincent Aboubakar",
+    img: "assets/images/VincentAboubakar.jpg"
+  },
+  {
+    name: "Vinicius Jr",
+    img: "assets/images/ViniciusJr.jpg"
+  },
+];
 
-  //Wait for the DOM to finish loading before running the game
-  //Get the button elements and add event listeners to them
+//Declare constants for DOM elements and possible choices
+const button = document.getElementById("startBtn");
+const quizContainer = document.getElementsByClassName("quiz-container");
+const quizBody = document.getElementsByClassName("quiz-body");
+const imgContainer = document.getElementsByClassName("image-container");
+const playerImage = document.getElementById("random-player");
+const myRadioButtons = document.getElementsByClassName("answer");
+const quizDetails = document.querySelector(".quiz-details");
 
-  document.addEventListener("DOMContentLoaded" function(){
-    let buttons = document.getElementsByTagName("button");
+const aText = document.getElementById("a-text");
+const bText = document.getElementById("b-text");
+const cText = document.getElementById("c-text");
+const dText = document.getElementById("d-text");
+const eText = document.getElementById("e-text");
 
-    for(let button of buttons){
-      if(this.getAttribute("") === "submit"){
-        checkAnswer();
-      }
-    }
-  })
+startBtn.addEventListener("click", playGame);
 
+function playGame() {
+  
+}
